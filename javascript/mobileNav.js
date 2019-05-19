7b9ef2af73e19d6a3ad17cacd8e0fcfdca97142b
Kyle Witeck <kylewiteck1@gmail.com>
2019-05-19 13:51:45 -0400

    added destop navigation and finished navigation

--- javascript/mobileNav.js
+++ javascript/mobileNav.js
@@ -4,11 +4,11 @@ const hamburgerIcon = document.querySelector('#hamburger-icon');
 const closeIcon = document.querySelector('#close-icon');
 const mobileMenuWrapper = document.querySelector('.mobile-menu-wrapper');
 const mobileMenu = document.querySelector('.mobile-menu');
+const mq = window.matchMedia('(min-width: 801)');
 
 hamburgerIcon.addEventListener('click', () => {
 	mobileMenu.style.animation = 'slideIn 1s ease-out';
 	mobileMenuWrapper.style.animation = 'backgroundBlack 1s ease-in';
-	mobileMenuWrapper.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
 	mobileMenuWrapper.style.display = 'block';
 });
 
@@ -17,5 +17,7 @@ closeIcon.addEventListener('click', () => {
 	mobileMenu.style.animation = 'slideOut 1s ease-out';
 	setTimeout(() => {
 		mobileMenuWrapper.style.display = 'none';
+		mobileMenuWrapper.removeAttribute('style');
+		mobileMenu.removeAttribute('style');
 	}, 800);
 });
