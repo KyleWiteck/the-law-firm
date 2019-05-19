bf27679e6b155ec6b7b76263974bde6d44a600c7
Kyle Witeck <kylewiteck1@gmail.com>
2019-05-19 11:06:24 -0400

    made the mobile nav drawer operatte smother

--- javascript/mobileNav.js
+++ javascript/mobileNav.js
@@ -5,17 +5,17 @@ const closeIcon = document.querySelector('#close-icon');
 const mobileMenuWrapper = document.querySelector('.mobile-menu-wrapper');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-hamburgerIcon.addEventListener('click', function() {
+hamburgerIcon.addEventListener('click', () => {
 	mobileMenu.style.animation = 'slideIn 1s ease-out';
 	mobileMenuWrapper.style.animation = 'backgroundBlack 1s ease-in';
 	mobileMenuWrapper.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
 	mobileMenuWrapper.style.display = 'block';
 });
 
-closeIcon.addEventListener('click', function() {
+closeIcon.addEventListener('click', () => {
 	mobileMenuWrapper.style.animation = 'backgroundNone 1s ease-out';
 	mobileMenu.style.animation = 'slideOut 1s ease-out';
 	setTimeout(() => {
 		mobileMenuWrapper.style.display = 'none';
-	}, 1000);
+	}, 800);
 });
