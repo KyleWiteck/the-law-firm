57121a8d8111a17f0124c79f05ed1b8f6d1f644b
Kyle Witeck <kylewiteck1@gmail.com>
2019-05-19 21:06:06 -0400

    added home here content and slide in animation

---
+++ javascript/homePage.js
@@ -0,0 +1,5 @@
+const homeHeroContentWrapper.style.animation = 'heroContentSlideIn 1s ease-in' = document.querySelector('#home-hero-content-wrapper');
+
+setTimeout(() => {
+    homeHeroContentWrapper.style.animation = 'heroContentSlideIn 1s ease-in';
+}, 500);
