e6b9479d4068c6250a3907fe0604c8dad534f1e8
Kyle Witeck <kylewiteck1@gmail.com>
2019-05-30 16:41:39 -0400

    added function for large back to top button to homepage.js

--- javascript/homePage.js
+++ javascript/homePage.js
@@ -1,16 +1,15 @@
 const practicesH2 = document.querySelector('#practices');
 const attorneyContentUl = document.querySelector('#attorney-content ul');
+const backToTopLg = document.querySelector('#back-to-top');
 
 // Adds fade in animation for larger screen sizes for specific elements
 const fadeIn = () => {
 	if (window.matchMedia('(min-width: 600px)').matches) {
 		window.addEventListener('scroll', e => {
-			console.log(window.scrollY);
 			if (
 				window.scrollY >=
 				attorneyContentUl.getBoundingClientRect().top + 400
 			) {
-				console.log('working');
 				attorneyContentUl.style.animation = 'fadeIn 2s ease-in';
 				attorneyContentUl.style.opacity = '1';
 			}
@@ -19,9 +18,7 @@ const fadeIn = () => {
 
 	if (window.matchMedia('(min-width: 600px)').matches) {
 		window.addEventListener('scroll', e => {
-			console.log(window.scrollY);
-			if (window.scrollY >= practicesH2.getBoundingClientRect().top - 400) {
-				console.log('working');
+			if (window.scrollY >= practicesH2.getBoundingClientRect().top - 100) {
 				practicesH2.style.animation = 'fadeIn 2s ease-in';
 				practicesH2.style.opacity = '1';
 			}
@@ -30,3 +27,12 @@ const fadeIn = () => {
 };
 
 fadeIn();
+
+// This handles the back to top button at the bottom of the page
+
+const scrollToTop = () => {
+	backToTopLg.addEventListener('click', () => {
+		window.scrollTo(0, 0);
+	});
+};
+scrollToTop();
