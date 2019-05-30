7210195b3a9c22914e7438f60adba16214fd967b
Kyle Witeck <kylewiteck1@gmail.com>
2019-05-30 16:33:18 -0400

    added scrolling animation to homepage.js

--- javascript/homePage.js
+++ javascript/homePage.js
@@ -1,5 +1,32 @@
-// const homeHeroContentWrapper= document.querySelector('#home-hero-content-wrapper');
+const practicesH2 = document.querySelector('#practices');
+const attorneyContentUl = document.querySelector('#attorney-content ul');
 
-// setTimeout(() => {
-//     homeHeroContentWrapper.style.animation = 'heroContentSlideIn 1s ease-in';
-// }, 500);
+// Adds fade in animation for larger screen sizes for specific elements
+const fadeIn = () => {
+	if (window.matchMedia('(min-width: 600px)').matches) {
+		window.addEventListener('scroll', e => {
+			console.log(window.scrollY);
+			if (
+				window.scrollY >=
+				attorneyContentUl.getBoundingClientRect().top + 400
+			) {
+				console.log('working');
+				attorneyContentUl.style.animation = 'fadeIn 2s ease-in';
+				attorneyContentUl.style.opacity = '1';
+			}
+		});
+	}
+
+	if (window.matchMedia('(min-width: 600px)').matches) {
+		window.addEventListener('scroll', e => {
+			console.log(window.scrollY);
+			if (window.scrollY >= practicesH2.getBoundingClientRect().top - 400) {
+				console.log('working');
+				practicesH2.style.animation = 'fadeIn 2s ease-in';
+				practicesH2.style.opacity = '1';
+			}
+		});
+	}
+};
+
+fadeIn();
---
+++ toDo.md
@@ -0,0 +1,6 @@
+## To Do Items
+
+- [] fix hamberger menu to close when item is clicked
+- [] finish all slide in animation
+- [] finish al fade in animation
+- [] add scroll to top feature fixed when scrolling, this must show up when nav bar is not visable and back to top button is not visable
